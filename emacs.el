0fdbbdff299bb205200bfa8e94b2153f5a2f2d7c
Paulo Lieuthier <paulolieuthier@gmail.com>
2016-10-24 01:27:38 -0300

    emacs: use irony for completion, rtags for go-to only

--- emacs.el
+++ emacs.el
@@ -96,27 +96,47 @@
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (global-company-mode))
 
+(use-package irony
+  :ensure t
+  :diminish irony-mode
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+
+  ;; replace the `completion-at-point' and `complete-symbol' bindings
+  ;in irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+(use-package company-irony
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-irony))
+
+(use-package flycheck-irony
+  :ensure t
+  :config
+  (flycheck-irony-setup))
+
 (use-package rtags
   :ensure t
   :config
-  (require 'company)
-  (require 'flycheck-rtags)
-
-  (setq rtags-autostart-diagnostics t)
-  (rtags-diagnostics)
-  (setq rtags-completions-enabled t)
-
-  (push 'company-rtags company-backends))
+  (add-hook 'c-mode-common-hook 'rtags-start-process-unless-running)
+  (add-hook 'c++-mode-common-hook 'rtags-start-process-unless-running)
+  (define-key evil-normal-state-map (kbd "gd") 'rtags-find-symbol-at-point))
 
 (use-package cmake-ide
   :ensure t
   :config
   (defun my-cmake-ide-hook ()
-    (require 'cl)
-    (require 'rtags)
     (setq cmake-ide-build-dir "./build")
-    (cmake-ide-setup)
-    (define-key evil-normal-state-map (kbd "gd") 'rtags-find-symbol-at-point))
+    (cmake-ide-setup))
 
   (add-hook 'c++-mode-hook 'my-cmake-ide-hook)
   (add-hook 'c-mode-hook 'my-cmake-ide-hook))
