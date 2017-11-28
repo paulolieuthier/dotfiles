579caf6f519c57b035103dbedb32bd63222aac22
Paulo Lieuthier <paulolieuthier@gmail.com>
2017-11-28 11:20:19 -0300

    install.py: do not backup files if they are already correct symlinks

--- install.py
+++ install.py
@@ -6,17 +6,21 @@ import shutil
 
 pwd = os.path.dirname(os.path.realpath(__file__))
 
-def install_config_file(config_file_name, symlink_name):
+def install_config_file(file_name, symlink_name):
     full_symlink_name = os.path.expanduser(symlink_name)
+    full_config_name = os.path.realpath(pwd + "/" + file_name)
 
     if os.path.exists(full_symlink_name):
-        shutil.move(full_symlink_name, full_symlink_name + ".old")
+        if os.path.samefile(full_symlink_name, full_config_name):
+            return
+        else:
+            shutil.move(full_symlink_name, full_symlink_name + ".old")
     else:
         dirname = os.path.dirname(full_symlink_name)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
 
-    os.symlink(pwd + "/" + config_file_name, full_symlink_name)
+    os.symlink(full_config_name, full_symlink_name)
 
 install_config_file("zshrc", "~/.zshrc")
 install_config_file("emacs.el", "~/.emacs.d/init.el")
