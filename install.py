fb8a986e4d440db2a798507da0cc7f6f0050b0e2
Paulo Lieuthier <paulolieuthier@gmail.com>
2018-05-28 08:45:32 -0300

    git: add git config file

--- install.py
+++ install.py
@@ -24,6 +24,7 @@ def install_config_file(file_name, symlink_name):
 
 install_config_file("zshrc", "~/.zshrc")
 install_config_file("tigrc", "~/.tigrc")
+install_config_file("gitconfig", "~/.gitconfig")
 install_config_file("emacs.el", "~/.emacs.d/init.el")
 install_config_file("vimrc", "~/.config/nvim/init.vim")
 install_config_file("Xmodmap", "~/.Xmodmap")
