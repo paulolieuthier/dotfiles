8114a61067fea6617ac343dfe439d8ebfc1921ef
Paulo Lieuthier <paulolieuthier@gmail.com>
2025-07-08 11:46:16 -0300

    nvim: treesitter: enable incremental config

--- nvim.lua
+++ nvim.lua
@@ -147,6 +147,15 @@ require('lazy').setup({
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
         },
     },
 
