43fc7b92b9927127206939e90a2c1c3119dbcea5
Paulo Lieuthier <paulolieuthier@gmail.com>
2026-02-19 13:22:25 -0300

    nvim: setup neotest and nvim-dap-ui

--- nvim.lua
+++ nvim.lua
@@ -288,4 +288,58 @@ require('lazy').setup({
             signature = { enabled = true },
         },
     },
+
+    {
+        'nvim-neotest/neotest',
+        dependencies = {
+            'nvim-neotest/nvim-nio',
+            'nvim-treesitter/nvim-treesitter',
+            'fredrikaverpil/neotest-golang',
+        },
+        config = function()
+            require('neotest').setup({
+                adapters = {
+                    require('neotest-golang') {
+                        runner = 'gotestsum',
+                        testify_enabled = true,
+                    },
+                },
+            })
+        end,
+    },
+
+    {
+        'rcarriga/nvim-dap-ui',
+        dependencies = {
+            'mfussenegger/nvim-dap',
+            'theHamsta/nvim-dap-virtual-text',
+            'leoluz/nvim-dap-go',
+        },
+        config = function()
+            local dap, dapui = require('dap'), require('dapui')
+
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+
+            vim.keymap.set('n', '<F5>', function() dap.continue() end)
+            vim.keymap.set('n', '<F8>', function() dap.step_over() end)
+            vim.keymap.set('n', '<F7>', function() dap.step_into() end)
+            vim.keymap.set('n', '<F8>', function() dap.step_out() end)
+            vim.keymap.set('n', '<M-b>', function() dap.toggle_breakpoint() end)
+            vim.keymap.set('n', '<M-e>', function() dapui.eval() end)
+
+            require('nvim-dap-virtual-text').setup()
+            require('dap-go').setup()
+        end,
+    },
 })
