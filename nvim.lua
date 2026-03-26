46ab8d489d10fca490b202ed04e26698b4f3f2a0
Paulo Lieuthier <paulolieuthier@gmail.com>
2026-03-26 19:40:57 -0300

    nvim: add leap.nvim plugin

--- nvim.lua
+++ nvim.lua
@@ -76,6 +76,14 @@ require('lazy').setup({
 	},
 
     {
+        'https://codeberg.org/andyg/leap.nvim',
+        config = function()
+            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
+            vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
+        end
+    },
+
+    {
         "christoomey/vim-tmux-navigator",
         cmd = {
             "TmuxNavigateLeft",
