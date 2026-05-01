b402af979583c5da026a7cbd9b0ec4b05fcc368d
Paulo Lieuthier <paulolieuthier@gmail.com>
2026-05-01 10:56:06 -0300

    nvim: leap: change keymap to avoid conflict with mini.surround

--- nvim.lua
+++ nvim.lua
@@ -90,8 +90,8 @@ require('lazy').setup({
     {
         'https://codeberg.org/andyg/leap.nvim',
         config = function()
-            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
-            vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
+            vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-forward)')
+            vim.keymap.set({'n', 'x', 'o'}, 'gS', '<Plug>(leap-backward)')
         end
     },
 
